0a8d6b33ba6d0f2ef0ad7a5a29670d52239d9f4c
saravana kumar <saravanakumar.ravichandran@codingmart.com>
2020-01-04 21:15:06 +0530

    Updates

--- precache-manifest.13a86764303d2fba15960804ac897849.js
+++ precache-manifest.13a86764303d2fba15960804ac897849.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26bded84447f6b277ce693a8c1c4e4ee",
+    "revision": "3c3835c67767dd5e4aeb1049495adc7b",
     "url": "/online-cbs-frontend/index.html"
   },
   {
-    "revision": "6e0494e5673a50b059cb",
+    "revision": "0fec89dba5309f5cae2f",
     "url": "/online-cbs-frontend/static/css/2.35f98baf.chunk.css"
   },
   {
-    "revision": "e0b47a7c9309dc2b321b",
+    "revision": "5beecf832dcc5a3a362b",
     "url": "/online-cbs-frontend/static/css/main.eab24d3c.chunk.css"
   },
   {
-    "revision": "6e0494e5673a50b059cb",
-    "url": "/online-cbs-frontend/static/js/2.b28d45e0.chunk.js"
+    "revision": "0fec89dba5309f5cae2f",
+    "url": "/online-cbs-frontend/static/js/2.c239ff46.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/online-cbs-frontend/static/js/2.b28d45e0.chunk.js.LICENSE"
+    "url": "/online-cbs-frontend/static/js/2.c239ff46.chunk.js.LICENSE"
   },
   {
-    "revision": "e0b47a7c9309dc2b321b",
-    "url": "/online-cbs-frontend/static/js/main.0fd10f90.chunk.js"
+    "revision": "5beecf832dcc5a3a362b",
+    "url": "/online-cbs-frontend/static/js/main.b4bd81b3.chunk.js"
   },
   {
     "revision": "cddf7309740fff56433c",
