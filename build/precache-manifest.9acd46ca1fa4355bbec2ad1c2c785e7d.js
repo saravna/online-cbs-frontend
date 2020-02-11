e75e1bb720c256b8739585b1faeefda88a77d943
saravana <saravanakumar.ravichandran@codingmart.com>
2020-02-11 16:29:25 +0530

    build

---
+++ build/precache-manifest.9acd46ca1fa4355bbec2ad1c2c785e7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ab7ae3a9eda25e0f32daa056ee9624",
+    "url": "/online-cbs-frontend/index.html"
+  },
+  {
+    "revision": "65f90d565aaf91eeafcb",
+    "url": "/online-cbs-frontend/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "73ddf52b1915dea4528a",
+    "url": "/online-cbs-frontend/static/css/main.c2ae10c8.chunk.css"
+  },
+  {
+    "revision": "65f90d565aaf91eeafcb",
+    "url": "/online-cbs-frontend/static/js/2.1be315a3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/online-cbs-frontend/static/js/2.1be315a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73ddf52b1915dea4528a",
+    "url": "/online-cbs-frontend/static/js/main.b13f0385.chunk.js"
+  },
+  {
+    "revision": "cddf7309740fff56433c",
+    "url": "/online-cbs-frontend/static/js/runtime-main.866ed493.js"
+  }
+]);
