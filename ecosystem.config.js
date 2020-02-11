b075343638d2dc3beb59eaff8cf1309d0a9938e0
saravana <saravanakumar.ravichandran@codingmart.com>
2020-02-11 17:10:13 +0530

    build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'FRONTEND',
     script: 'reactServer.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
