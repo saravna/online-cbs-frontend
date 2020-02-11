ef2498e6a98153bd0b0f16a95598a1545b5b96c1
saravana <saravanakumar.ravichandran@codingmart.com>
2020-02-11 16:15:51 +0530

    build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'reactServer.js',
+    // script: 'reactServer.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
