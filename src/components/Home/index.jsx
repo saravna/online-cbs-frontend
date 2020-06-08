32db2552663d7de487fc5c9ef2a5bd6dcce32c9a
saravana <saravanakumar.ravichandran@codingmart.com>
2020-06-08 15:23:17 +0530

    ad trial

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -229,27 +229,6 @@ const Home = (props) => {
                     </Form>
                 </Modal>
             </Content>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            {/* <!-- login page ad --> */}
-            <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-9122830473874434"
-                data-ad-slot="5385024866"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            {/* <script> */}
-                {(adsbygoogle = window.adsbygoogle || []).push({})};
-            {/* </script> */}
-            {/* <!-- login page ad --> */}
-            <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-9122830473874434"
-                data-ad-slot="5385024866"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            {/* <script> */}
-                {(adsbygoogle = window.adsbygoogle || []).push({})};
-            {/* </script> */}
         </div>
      );
 }
